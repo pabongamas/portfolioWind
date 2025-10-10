2cd8d6eeda44dd2fda32f490af2e598efd698564
pabongamas <72658365+pabongamas@users.noreply.github.com>
2025-10-10 23:32:50 +1100

    first  commit

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -4,22 +4,22 @@ import "./../globals.css";
 
 import { Sorts_Mill_Goudy } from 'next/font/google'
 
-const Sorts = Sorts_Mill_Goudy({
-  variable: "--font-Sorts-Mill",
-  weight: "400",
-  subsets: ["latin"],
-  display: "swap",
-});
+// const Sorts = Sorts_Mill_Goudy({
+//   variable: "--font-Sorts-Mill",
+//   weight: "400",
+//   subsets: ["latin"],
+//   display: "swap",
+// });
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
